bd05b233454adca02d6f1640055a1e9fb2a6ee7f
Sorokin Alexander <uas.sorokin@gmail.com>
2014-04-24 00:05:58 +0400

    Interface for BLL

--- bll/src/main.js
+++ bll/src/main.js
@@ -1,3 +1,42 @@
 /**
  * Created by UAS on 23.04.2014.
  */
+
+var Wrapper = function(DAL) {
+    this.dal = DAL;
+};
+
+Wrapper.prototype.apps_list = function(args, next) {
+    return [];
+};
+
+Wrapper.prototype.hd_conversationsList = function(args, next) {
+    return [];
+};
+
+Wrapper.prototype.hd_messageCreate = function(args, next) {
+    return {};
+};
+
+Wrapper.prototype.hd_messagesList = function(args, next) {
+    return [];
+};
+
+Wrapper.prototype.security_createAuthToken = function(args, next) {
+    return {};
+};
+
+Wrapper.prototype.system_getTime = function(args, next) {
+    return {};
+};
+
+Wrapper.prototype.user_init = function(args, next) {
+    return null;
+};
+
+Wrapper.prototype.user_register = function(args, next) {
+
+};
+
+
+module.exports = Wrapper;
---
+++ bll/test/interface.js
@@ -0,0 +1,44 @@
+/**
+ * Created by UAS on 23.04.2014.
+ */
+
+var assert = require('chai').assert;
+
+describe('Interface', function() {
+    it('Check all interface functions are exists', function() {
+        var iReq = require('../');
+        var i = new iReq(null);
+
+        assert.isDefined(i.apps_list, 'apps_list function is not exists');
+        assert.isFunction(i.apps_list, 'apps_list must be a function');
+        assert.equal(i.apps_list.length, 2, 'apps_list must receive 2 arguments only');
+
+        assert.isDefined(i.hd_conversationsList, 'hd_conversationsList function is not exists');
+        assert.isFunction(i.hd_conversationsList, 'hd_conversationsList must be a function');
+        assert.equal(i.hd_conversationsList.length, 2, 'hd_conversationsList must receive 2 arguments only');
+
+        assert.isDefined(i.hd_messageCreate, 'hd_messageCreate function is not exists');
+        assert.isFunction(i.hd_messageCreate, 'hd_messageCreate must be a function');
+        assert.equal(i.hd_messageCreate.length, 2, 'hd_messageCreate must receive 2 arguments only');
+
+        assert.isDefined(i.hd_messagesList, 'hd_messagesList function is not exists');
+        assert.isFunction(i.hd_messagesList, 'hd_messagesList must be a function');
+        assert.equal(i.hd_messagesList.length, 2, 'hd_messagesList must receive 2 arguments only');
+
+        assert.isDefined(i.security_createAuthToken, 'security_createAuthToken function is not exists');
+        assert.isFunction(i.security_createAuthToken, 'security_createAuthToken must be a function');
+        assert.equal(i.security_createAuthToken.length, 2, 'security_createAuthToken must receive 2 arguments only');
+
+        assert.isDefined(i.system_getTime, 'system_getTime function is not exists');
+        assert.isFunction(i.system_getTime, 'system_getTime must be a function');
+        assert.equal(i.system_getTime.length, 2, 'system_getTime must receive 2 arguments only');
+
+        assert.isDefined(i.user_init, 'user_init function is not exists');
+        assert.isFunction(i.user_init, 'user_init must be a function');
+        assert.equal(i.user_init.length, 2, 'user_init must receive 2 arguments only');
+
+        assert.isDefined(i.user_register, 'user_register function is not exists');
+        assert.isFunction(i.user_register, 'user_register must be a function');
+        assert.equal(i.user_register.length, 2, 'user_register must receive 2 arguments only');
+    });
+});
