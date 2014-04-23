db68ac39edc146ae46cf949f5610bebecc74a941
Sorokin Alexander <uas.sorokin@gmail.com>
2014-04-24 01:12:01 +0400

    Import of old code for apps_list

--- bll/src/__apps_list.js
+++ bll/src/__apps_list.js
@@ -2,6 +2,100 @@
  * Created by UAS on 24.04.2014.
  */
 
+function getResult() {
+    return [];
+}
+
+
 module.exports = function(args, next) {
-    next(null, []);
+    next(null, getResult());
 };
+
+
+//var pg = require('pg');
+//var RpcErrors = require('./_RpcErrors');
+//var Common = require('./_Common');
+//var Platforms = require('./_Platforms');
+
+
+//[{
+//    "id": "1",
+//    "title": "Test app",
+//    "created": 1378243940,
+//    "is_agent": false,
+//    "number_of_conversations": 10,
+//    "number_of_unread_messages": 50
+//}, ...]
+/*var Clazz = function(args, rpc, env) {
+    this.args = args;
+    this.rpc = rpc;
+    this.cfg = env.cfg;
+};
+
+Clazz.prototype.execute = function() {
+    var self = this;
+    Common.connectToDb(this.cfg.db.dsn, this.rpc, function(client, done) {
+        Common.getUserIdByToken(client, self.args.auth_token, function(err, success) {
+            if (err) {
+                self.rpc.error(RpcErrors.INTERNAL_ERROR, err.toString(), err);
+                done();
+            } else {
+                if (success === '') {
+                    self.rpc.error(RpcErrors.USER_NOT_FOUND, "User with specified token is not found");
+                    done();
+                } else {
+                    self._extractApps(client, done, success);
+                }
+            }
+        });
+    });
+};
+
+Clazz.prototype._extractApps = function(client, done, userId) {
+    var rawQuery =
+        'SELECT a.id, a.title, extract ("epoch" from a.created)::int AS created, e.package, ' +
+        '(SELECT COUNT(*) FROM conversations c WHERE c.app_id = a.id)::int AS number_of_conversations, ' +
+        '(SELECT COUNT(*) FROM conversation_message_is_read cmr WHERE ' +
+        'user_id = $1 ' +
+        'AND is_read = FALSE ' +
+        'AND cmr.conversation_message_id IN( ' +
+        'SELECT cm.id FROM conversation_messages cm WHERE cm.conversation_id IN( ' +
+        'SELECT c1.id FROM conversations c1 WHERE c1.app_id = a.id)) ' +
+        ')::int ' +
+        'AS number_of_unread_messages '+
+        'FROM apps a, app_info_extra_android e ' +
+        'WHERE a.owner_user_id = $1 AND e.app_id = a.id';
+    var preparedQuery = {
+        name: 'apps.list.android',
+        text: rawQuery,
+        values: [userId]
+    };
+
+    var query = client.query(preparedQuery);
+    var self = this;
+
+    query.on('row', function(row, result) {
+        result.addRow({
+            id: row['id'],
+            title: row['title'],
+            created: row['created'],
+            platform_type: Platforms.ANDROID,
+            platform_extra: {
+                package: row['package']
+            },
+            number_of_conversations: row['number_of_conversations'],
+            number_of_unread_messages: row['number_of_unread_messages']
+        });
+    });
+    query.on('end', function(result) {
+        self.rpc.success(result.rows);
+        done();
+    });
+    query.on('error', function(err) {
+        self.rpc.error(RpcErrors.INTERNAL_ERROR, err.toString(), err);
+        done();
+    });
+};
+
+
+module.exports = Clazz;*/
