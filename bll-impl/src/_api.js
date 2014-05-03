63c7de493dfb4dffa006a3cc2624303ac1236602
Sorokin Alexander <uas.sorokin@gmail.com>
2014-05-04 00:16:07 +0400

    Api delegates files renaming

--- bll-impl/src/_api.js
+++ bll-impl/src/_api.js
@@ -4,14 +4,14 @@
 
 "use strict";
 
-var __apps_list = require('./__apps_list');
-var __hd_conversationsList = require('./__hd_conversationsList');
-var __hd_messageCreate = require('./__hd_messageCreate');
-var __hd_messagesList = require('./__hd_messagesList');
-var __security_createAuthToken = require('./__security_createAuthToken');
-var __system_getTime = require('./__system_getTime');
-var __users_init = require('./__users_init');
-var __users_register = require('./__users_register');
+var __apps_list = require('./_api_apps_list');
+var __hd_conversationsList = require('./_api_hd_conversationsList');
+var __hd_messageCreate = require('./_api_hd_messageCreate');
+var __hd_messagesList = require('./_api_hd_messagesList');
+var __security_createAuthToken = require('./_api_security_createAuthToken');
+var __system_getTime = require('./_api_system_getTime');
+var __users_init = require('./_api_users_init');
+var __users_register = require('./_api_users_register');
 
 
 var Api = function(DAL) {
@@ -19,35 +19,35 @@ var Api = function(DAL) {
 };
 
 Api.prototype.apps_list = function(args, next) {
-    __apps_list(this.dal, args, next);
+    _api_apps_list(this.dal, args, next);
 };
 
 Api.prototype.hd_conversationsList = function(args, next) {
-    __hd_conversationsList(args, next);
+    _api_hd_conversationsList(args, next);
 };
 
 Api.prototype.hd_messageCreate = function(args, next) {
-    __hd_messageCreate(args, next);
+    _api_hd_messageCreate(args, next);
 };
 
 Api.prototype.hd_messagesList = function(args, next) {
-    __hd_messagesList(args, next);
+    _api_hd_messagesList(args, next);
 };
 
 Api.prototype.security_createAuthToken = function(args, next) {
-    __security_createAuthToken(args, next);
+    _api_security_createAuthToken(args, next);
 };
 
 Api.prototype.system_getTime = function(args, next) {
-    __system_getTime(args, next);
+    _api_system_getTime(args, next);
 };
 
 Api.prototype.users_init = function(args, next) {
-    __users_init(args, next);
+    _api_users_init(args, next);
 };
 
 Api.prototype.users_register = function(args, next) {
-    __users_register(args, next);
+    _api_users_register(args, next);
 };
 
 
