6ab7c8d0cf37302c3d715807586221030865feef
Sorokin Alexander <uas.sorokin@gmail.com>
2014-05-07 03:41:24 +0400

    bigint geneartion and tests are ready

--- uuid-generator/src/main.js
+++ uuid-generator/src/main.js
@@ -15,8 +15,30 @@ var Generator = function() {
     var _nodeId = -1;
     var _self = this;
     var _isInited = false;
+
+    var _startDateMillis = new Date('2014-01-01 00:00:00 +00:00').getTime();
     var _inc = 0;
     var _mod = 1024;
+
+    var _getTimeMillisFunctionDefault = function() {
+        return Date.now() - _startDateMillis;
+    };
+
+    var _reserved = '0000';
+    var _zerofills = {};
+    var _nodeIdAsString = '';
+    var _getTimeMillisFunction = _getTimeMillisFunctionDefault;
+    var _pad = function(str, padToSize) {
+        if (str.length === padToSize) {
+            return str;
+        } else {
+            return _zerofills[padToSize - str.length] + str;
+        }
+    };
+
+    this.overrideGetTimeMillisFunction = function(fn) {
+        _getTimeMillisFunction = fn;
+    };
 
     this.init = function(nodeId, done) {
         if (_isInited) {
@@ -33,9 +55,21 @@ var Generator = function() {
             return done(new Error('nodeId is greater than maxNodeId'));
         }
 
+        if (Date.now() < _startDateMillis) {
+            return done(new Error('Current PC time is invalid. It must be greater than ' + _startDateMillis));
+        }
+
         setTimeout(function() {
             _isInited = true;
+
+            _zerofills[1] = '0';
+            for (var i = 2; i <= 41; i++) {
+                _zerofills[i] = '0' + _zerofills[i-1];
+            }
+
             _nodeId = nodeId;
+            _nodeIdAsString = _pad(_nodeId.toString(2), 8);
+
             done(null);
         }, _delayBeforeStart);
     };
@@ -46,14 +80,19 @@ var Generator = function() {
         }
 
         _inc = (_inc + 1) % _mod;
-        return done(null, _inc);
+
+        var millis = _pad(_getTimeMillisFunction().toString(2), 42);
+        var incAsStr = _pad(_inc.toString(2), 10);
+        var r = new BigNumber(millis + _reserved + _nodeIdAsString + incAsStr, 2);
+
+        return done(null, r.toString(10));
     };
 
     this.newGuid = function(done) {
         if (!_isInited) {
             return done(new Error('#init did not called'));
         }
-        done(null, '');
+        done(null, 'x');
     };
 };
 
--- uuid-generator/test/test.js
+++ uuid-generator/test/test.js
@@ -5,6 +5,7 @@
 "use strict";
 
 var assert = require('chai').assert;
+var BigNumber = require('bignumber.js');
 
 var genDef = require('../');
 
@@ -34,13 +35,14 @@ describe('Interface', function() {
 
         assert.property(gen, 'newGuid', 'newGuid method is not defined');
         assert.isFunction(gen.newGuid, 'newGuid method is not a function');
+
+        assert.property(gen, 'overrideGetTimeMillisFunction', 'overrideGetTimeMillisFunction method is not defined');
+        assert.isFunction(gen.overrideGetTimeMillisFunction, 'overrideGetTimeMillisFunction method is not a function');
     });
 });
 
 
 describe('Logic', function() {
-
-    var nodeId = 1;
 
     describe('#init', function() {
         it('Should accept only numbers', function(doneTest) {
@@ -111,9 +113,30 @@ describe('Logic', function() {
             });
         });
 
+        var split = function(value) {
+            var num = new BigNumber(value);
+            var numBinary = num.toString(2);
+            for (var i = numBinary.length; i < 64; i++) {
+                numBinary = '0' + numBinary;
+            }
+
+            return [
+                numBinary.substr(0, 42),
+                numBinary.substr(42, 4),
+                numBinary.substr(46, 8),
+                numBinary.substr(54)
+            ];
+        };
+
         it('Check result', function(doneTest) {
             var gen = new genDef();
-            gen.init(gen.minNodeId, function(errInit) {
+            var nodeId = gen.minNodeId;
+
+            gen.overrideGetTimeMillisFunction(function() {
+                return 1;
+            });
+
+            gen.init(nodeId, function(errInit) {
                 if (errInit) {
                     return doneTest(errInit);
                 }
@@ -121,15 +144,105 @@ describe('Logic', function() {
                     if (err) {
                         return doneTest(err);
                     }
+
                     assert.typeOf(result, 'string', 'The result of #newBigInt must be a string');
                     assert.operator(result.length, '>', 0, 'The length of result for #newBigInt must be a greater than 0');
+                    assert.operator(result.length, '<=', 19, 'The length of result for #newBigInt must be a lower or equal 19');
+
+                    var num = new BigNumber(result);
+                    assert.ok(num.greaterThanOrEqualTo(0), 'Result exceeds the minimal value of 0');
+                    assert.ok(num.lessThanOrEqualTo('9223372036854775807', 10), 'Result exceeds the maximum value of 9223372036854775807');
+
+                    var splitted = split(result);
+
+                    var millisBinary = splitted[0];
+                    var reservedBinary = splitted[1];
+                    var nodeBinary = splitted[2];
+
+                    assert.ok(new BigNumber(millisBinary, 2).comparedTo(1) === 0, 'Bits for millis are not match');
+                    assert.strictEqual(reservedBinary, '0000', 'Reserved bits should be 4-length zeroes');
+                    assert.ok(new BigNumber(nodeBinary).comparedTo(nodeId) === 0, 'Bits for node are not match');
+
                     doneTest();
                 });
             });
         });
+
+        it('Check result is incrementing', function(doneTest) {
+            var gen = new genDef();
+            var nodeId = gen.minNodeId;
+
+            gen.overrideGetTimeMillisFunction(function() {
+                return 1;
+            });
+
+            gen.init(nodeId, function(errInit) {
+                if (errInit) {
+                    return doneTest(errInit);
+                }
+
+                gen.newBigInt(function(errFirst, resultFirst) {
+                    if (errFirst) {
+                        return doneTest(errFirst);
+                    }
+
+                    var first = parseInt(split(resultFirst)[3], 2);
+
+                    gen.newBigInt(function(errSecond, resultSecond) {
+                        if (errSecond) {
+                            return doneTest(errSecond);
+                        }
+
+                        var second = parseInt(split(resultSecond)[3], 2);
+                        assert.strictEqual(second - first, 1, 'Difference between increments must be equal to 1');
+                        doneTest();
+                    });
+                });
+            });
+        });
+
+        it('Check increment is switched to 0 after 1026 calls', function(doneTest) {
+            var gen = new genDef();
+            var nodeId = gen.minNodeId;
+
+            gen.overrideGetTimeMillisFunction(function() {
+                return 1;
+            });
+
+            gen.init(nodeId, function(errInit) {
+                if (errInit) {
+                    return doneTest(errInit);
+                }
+
+                var i = 0;
+                var end = 1026;
+
+                var iterationsFinishCallback = function(result) {
+                    var inc = parseInt(split(result)[3], 2);
+                    assert.strictEqual(inc, 2, 'After 1026 iterations it is expected that inc will be equal to 1');
+                    doneTest();
+                };
+
+                var iteration = function() {
+                    gen.newBigInt(function(err, result) {
+                        if (err) {
+                            return doneTest(err);
+                        }
+
+                        i++;
+                        if (i === end) {
+                            iterationsFinishCallback(result);
+                        } else {
+                            iteration();
+                        }
+                    });
+                };
+                iteration();
+            });
+        });
     });
 
-    describe.only('#newGuid', function() {
+    describe('#newGuid', function() {
         it('Should not be working before call of #init', function(doneTest) {
             var gen = new genDef();
             gen.newGuid(function(err, result) {
