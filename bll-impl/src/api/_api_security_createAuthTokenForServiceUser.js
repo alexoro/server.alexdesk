309aaceb82c1a421e2e96254256b55fb99a34cb0
Sorokin Alexander <uas.sorokin@gmail.com>
2014-05-30 01:43:13 +0400

    security_createAuthTokenForServiceUser refactoring

--- bll-impl/src/api/_api_security_createAuthTokenForServiceUser.js
+++ bll-impl/src/api/_api_security_createAuthTokenForServiceUser.js
@@ -5,129 +5,161 @@
 "use strict";
 
 
-var _ = require('underscore');
 var async = require('async');
 
 var domain = require('../domain');
+var dErr = domain.errors;
 
 var errBuilder = require('./_errorBuilder');
 var validate = require('./_validation');
 
 
-var _validateArgsHasErrors = function(env, args) {
-    var dErr = domain.errors;
-
-    if (!args) {
-        return errBuilder(dErr.INVALID_PARAMS, 'Arguments is not defined');
-    }
-    if (typeof args !== 'object') {
-        return errBuilder(dErr.INVALID_PARAMS, 'Arguments is not a object');
-    }
-    if (args.login === undefined || args.password === undefined) {
-        return errBuilder(dErr.INVALID_PARAMS, 'Not all required fields are set: login or password');
-    }
-
-    if (!validate.email(args.login)) {
-        return errBuilder(dErr.INVALID_PARAMS, 'Service user login must be in email format');
-    }
-    if (!validate.serviceUserPassword(args.password)) {
-        return errBuilder(dErr.INVALID_PARAMS, 'Password must be a string with length [1, 64]');
-    }
-};
-
-var _create = function(env, args, next) {
-    var dal = env.dal;
-    var uuid = env.uuid;
-    var dUserTypes = domain.userTypes;
-    var dErr = domain.errors;
-
+var fnExecute = function (env, args, next) {
     var fnStack = [
         function(cb) {
-            env.passwordManager.hashServiceUserPassword(args.password, cb);
-        },
-        function(passwordHash, cb) {
-            var creditionals = {
-                login: args.login,
-                passwordHash: passwordHash
+            var flow = {
+                args: args,
+                env: env,
+                passwordHash: null,
+                userId: null,
+                tokenId: null,
+                tokenExpires: null,
+                result: null
             };
-            dal.getServiceUserIdByCreditionals(creditionals, function(err, userId) {
-                if (err) {
-                    cb(errBuilder(dErr.INTERNAL_ERROR, err));
-                } else if (!userId) {
-                    cb(errBuilder(dErr.USER_NOT_FOUND, 'User with specified creditionals is not found'));
-                } else {
-                    cb(null, userId);
-                }
-            });
+            cb(null , flow);
         },
-        function (userId, cb) {
-            var reqArgs = {
-                userId: userId
-            };
-            dal.serviceUserIsConfirmed(reqArgs, function (err, isConfirmed) {
-                if (err) {
-                    cb(errBuilder(dErr.INTERNAL_ERROR, err));
-                } else if (!isConfirmed) {
-                    cb(errBuilder(dErr.USER_NOT_CONFIRMED, 'User not confirmed'));
-                } else {
-                    cb(null, userId);
-                }
-            });
-        },
-
-        function(userId, cb) {
-            uuid.newGuid4(function(err, guid) {
-                if (err) {
-                    cb(errBuilder(dErr.INTERNAL_ERROR, err));
-                } else {
-                    cb(null, userId, guid);
-                }
-            });
-        },
-        function(userId, guid, cb) {
-            env.accessTokenConfig.getExpireTimeForServiceUser(function(err, expires) {
-                if (err) {
-                    cb(errBuilder(dErr.INTERNAL_ERROR, err));
-                } else {
-                    cb(null, userId, guid, expires);
-                }
-            });
-        },
-        function(userId, guid, expires, cb) {
-            var toSave = {
-                token: guid,
-                userType: dUserTypes.SERVICE_USER,
-                userId: userId,
-                expires: expires
-            };
-            dal.createAuthToken(toSave, function(err) {
-                if (err) {
-                    cb(errBuilder(dErr.INTERNAL_ERROR, err));
-                } else {
-                    cb(null, guid, expires);
-                }
-            });
-        }
+        fnValidate,
+        fnServiceUserHashPassword,
+        fnServiceUserGetIdByCreditionals,
+        fnServiceUserIsConfirmed,
+        fnTokenGenerateId,
+        fnTokenGenerateExpireTime,
+        fnTokenSave,
+        fnGenerateResult
     ];
 
     async.waterfall(
         fnStack,
-        function(err, guid, expires) {
+        function(err, flow) {
             if (err) {
-                return next(err);
+                next(err);
             } else {
-                next(null, {token: guid, expires: expires});
+                next(null, flow.result);
             }
         }
     );
 };
 
 
-module.exports = function(env, args, next) {
-    var argsError = _validateArgsHasErrors(env, args);
-    if (argsError) {
-        next(argsError, null);
-    } else {
-        _create(env, args, next);
+var fnValidate = function (flow, cb) {
+    if (!flow.args) {
+        return cb(errBuilder(dErr.INVALID_PARAMS, 'Arguments is not defined'));
     }
+    if (typeof flow.args !== 'object') {
+        return cb(errBuilder(dErr.INVALID_PARAMS, 'Arguments is not a object'));
+    }
+    if (flow.args.login === undefined || flow.args.password === undefined) {
+        return cb(errBuilder(dErr.INVALID_PARAMS, 'Not all required fields are set: login or password'));
+    }
+
+    if (!validate.email(flow.args.login)) {
+        return cb(errBuilder(dErr.INVALID_PARAMS, 'Service user login must be in email format'));
+    }
+    if (!validate.serviceUserPassword(flow.args.password)) {
+        return cb(errBuilder(dErr.INVALID_PARAMS, 'Password must be a string with length [1, 64]'));
+    }
+
+    return cb(null, flow);
 };
+
+var fnServiceUserHashPassword = function (flow, cb) {
+    flow.env.passwordManager.hashServiceUserPassword(flow.args.password, function(err, hashedPassword) {
+        if (err) {
+            cb(errBuilder(dErr.INTERNAL_ERROR, err));
+        } else {
+            flow.passwordHash = hashedPassword;
+            cb(null, flow);
+        }
+    });
+};
+
+var fnServiceUserGetIdByCreditionals = function (flow, cb) {
+    var reqArgs = {
+        login: flow.args.login,
+        passwordHash: flow.passwordHash
+    };
+    flow.env.dal.getServiceUserIdByCreditionals(reqArgs, function(err, userId) {
+        if (err) {
+            cb(errBuilder(dErr.INTERNAL_ERROR, err));
+        } else if (!userId) {
+            cb(errBuilder(dErr.USER_NOT_FOUND, 'User with specified creditionals is not found'));
+        } else {
+            flow.userId = userId;
+            cb(null, flow);
+        }
+    });
+};
+
+var fnServiceUserIsConfirmed = function (flow, cb) {
+    var reqArgs = {
+        userId: flow.userId
+    };
+    flow.env.dal.serviceUserIsConfirmed(reqArgs, function (err, isConfirmed) {
+        if (err) {
+            cb(errBuilder(dErr.INTERNAL_ERROR, err));
+        } else if (!isConfirmed) {
+            cb(errBuilder(dErr.USER_NOT_CONFIRMED, 'User not confirmed'));
+        } else {
+            cb(null, flow);
+        }
+    });
+};
+
+var fnTokenGenerateId = function (flow, cb) {
+    flow.env.uuid.newGuid4(function(err, guid) {
+        if (err) {
+            cb(errBuilder(dErr.INTERNAL_ERROR, err));
+        } else {
+            flow.tokenId = guid;
+            cb(null, flow);
+        }
+    });
+};
+
+var fnTokenGenerateExpireTime = function (flow, cb) {
+    flow.env.accessTokenConfig.getExpireTimeForServiceUser(function(err, expires) {
+        if (err) {
+            cb(errBuilder(dErr.INTERNAL_ERROR, err));
+        } else {
+            flow.tokenExpires = expires;
+            cb(null, flow);
+        }
+    });
+};
+
+var fnTokenSave = function (flow, cb) {
+    var toSave = {
+        token: flow.tokenId,
+        userType: domain.userTypes.SERVICE_USER,
+        userId: flow.userId,
+        expires: flow.tokenExpires
+    };
+    flow.env.dal.createAuthToken(toSave, function(err) {
+        if (err) {
+            cb(errBuilder(dErr.INTERNAL_ERROR, err));
+        } else {
+            cb(null, flow);
+        }
+    });
+};
+
+var fnGenerateResult = function (flow, cb) {
+    flow.result = {
+        token: flow.tokenId,
+        expires: flow.tokenExpires
+    };
+    cb(null, flow);
+};
+
+
+module.exports = fnExecute;
