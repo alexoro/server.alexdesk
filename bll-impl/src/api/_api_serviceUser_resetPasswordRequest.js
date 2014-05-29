d1b0858337323674a4489ca053a88bb70bf2e4b8
Sorokin Alexander <uas.sorokin@gmail.com>
2014-05-29 21:51:44 +0400

    resetPasswordRequest refactoring

--- bll-impl/src/api/_api_serviceUser_resetPasswordRequest.js
+++ bll-impl/src/api/_api_serviceUser_resetPasswordRequest.js
@@ -16,25 +16,7 @@ var validate = require('./_validation');
 var filter = require('./_filter');
 
 
-var _validateArgsHasErrors = function(env, args) {
-    var dErr = domain.errors;
-
-    if (!args) {
-        return errBuilder(dErr.INVALID_PARAMS, 'Arguments is not defined');
-    }
-    if (typeof args !== 'object') {
-        return errBuilder(dErr.INVALID_PARAMS, 'Arguments is not a object');
-    }
-
-    if (args.login === undefined) {
-        return errBuilder(dErr.INVALID_PARAMS, 'Login is not defined');
-    }
-    if (!validate.email(args.login)) {
-        return errBuilder(dErr.INVALID_PARAMS, 'Service user login must be in email format');
-    }
-};
-
-var _create = function(env, args, next) {
+var fnExecute = function(env, args, next) {
     var fnStack = [
         function(cb) {
             var flow = {
@@ -46,103 +28,129 @@ var _create = function(env, args, next) {
             };
             cb(null , flow);
         },
-
-        function (flow, cb) {
-            flow.env.dal.getServiceUserCreditionalsByLogin({login: flow.args.login}, function (err, creditionals) {
-                if (err) {
-                    cb(errBuilder(dErr.INTERNAL_ERROR, err));
-                } else if (!creditionals) {
-                    cb(errBuilder(dErr.USER_NOT_FOUND, 'User with specified login is not found'));
-                } else {
-                    flow.userId = creditionals.id;
-                    cb(null, flow);
-                }
-            });
-        },
-        function (flow, cb) {
-            flow.env.dal.serviceUserIsConfirmed({userId: flow.userId}, function (err, isConfirmed) {
-                if (err) {
-                    cb(errBuilder(dErr.INTERNAL_ERROR, err));
-                } else if (!isConfirmed) {
-                    cb(errBuilder(dErr.USER_NOT_CONFIRMED, 'User is not confirmed'));
-                } else {
-                    cb(null, flow);
-                }
-            });
-        },
-
-        function(flow, cb) {
-            flow.env.uuid.newGuid4(function(err, id) {
-                if (err) {
-                    cb(errBuilder(dErr.INTERNAL_ERROR, err));
-                } else {
-                    flow.confirmId = id;
-                    cb(null, flow);
-                }
-            });
-        },
-        function (flow, cb) {
-            flow.env.accessTokenConfig.getExpireTimeForPasswordReset(function (err, expires) {
-                if (err) {
-                    cb(errBuilder(dErr.INTERNAL_ERROR, err));
-                } else {
-                    flow.confirmExpires = expires;
-                    cb(null, flow);
-                }
-            });
-        },
-
-        function (flow, cb) {
-            var reqArgs = {
-                id: flow.confirmId,
-                userId: flow.userId,
-                expires: flow.confirmExpires
-            };
-            flow.env.dal.serviceUserCreateResetPasswordConfirmData(reqArgs, function (err) {
-                if (err) {
-                    cb(errBuilder(dErr.INTERNAL_ERROR, err));
-                } else {
-                    cb(null, flow);
-                }
-            });
-        },
-        function (flow, cb) {
-            var reqArgs = {
-                id: flow.confirmId,
-                expires: flow.confirmExpires
-            };
-            flow.env.emailSender.sendServiceUserResetPasswordConfirmLink(reqArgs, function(err) {
-                if (err) {
-                    cb(errBuilder(dErr.INTERNAL_ERROR, err));
-                } else {
-                    cb(null, flow);
-                }
-            });
-        }
+        fnValidate,
+        fnServiceUserInfoGetByLogin,
+        fnServiceUserisConfirmed,
+        fnGenerateConfirmId,
+        fnGenerateExpireTimeForConfirm,
+        fnServiceUserCreateConfirm,
+        fnSendConfirmInfo,
+        fnGenerateResponse
     ];
 
     async.waterfall(
         fnStack,
-        function(err, flow) {
+        function(err, result) {
             if (err) {
                 next(err);
             } else {
-                var r = {
-                    id: flow.confirmId,
-                    expires: flow.confirmExpires
-                };
-                next(null, r);
+                next(null, result);
             }
         }
     );
 };
 
-
-module.exports = function(env, args, next) {
-    var argsError = _validateArgsHasErrors(env, args);
-    if (argsError) {
-        next(argsError, null);
-    } else {
-        _create(env, args, next);
+var fnValidate = function (flow, cb) {
+    if (!flow.args) {
+        return cb(errBuilder(dErr.INVALID_PARAMS, 'Arguments is not defined'));
     }
+    if (typeof flow.args !== 'object') {
+        return cb(errBuilder(dErr.INVALID_PARAMS, 'Arguments is not a object'));
+    }
+
+    if (flow.args.login === undefined) {
+        return cb(errBuilder(dErr.INVALID_PARAMS, 'Login is not defined'));
+    }
+    if (!validate.email(flow.args.login)) {
+        return cb(errBuilder(dErr.INVALID_PARAMS, 'Service user login must be in email format'));
+    }
+
+    return cb(null, flow);
 };
+
+var fnServiceUserInfoGetByLogin = function (flow, cb) {
+    flow.env.dal.getServiceUserCreditionalsByLogin({login: flow.args.login}, function (err, creditionals) {
+        if (err) {
+            cb(errBuilder(dErr.INTERNAL_ERROR, err));
+        } else if (!creditionals) {
+            cb(errBuilder(dErr.USER_NOT_FOUND, 'User with specified login is not found'));
+        } else {
+            flow.userId = creditionals.id;
+            cb(null, flow);
+        }
+    });
+};
+
+var fnServiceUserisConfirmed = function (flow, cb) {
+    flow.env.dal.serviceUserIsConfirmed({userId: flow.userId}, function (err, isConfirmed) {
+        if (err) {
+            cb(errBuilder(dErr.INTERNAL_ERROR, err));
+        } else if (!isConfirmed) {
+            cb(errBuilder(dErr.USER_NOT_CONFIRMED, 'User is not confirmed'));
+        } else {
+            cb(null, flow);
+        }
+    });
+};
+
+var fnGenerateConfirmId = function (flow, cb) {
+    flow.env.uuid.newGuid4(function(err, id) {
+        if (err) {
+            cb(errBuilder(dErr.INTERNAL_ERROR, err));
+        } else {
+            flow.confirmId = id;
+            cb(null, flow);
+        }
+    });
+};
+
+var fnGenerateExpireTimeForConfirm = function (flow, cb) {
+    flow.env.accessTokenConfig.getExpireTimeForPasswordReset(function (err, expires) {
+        if (err) {
+            cb(errBuilder(dErr.INTERNAL_ERROR, err));
+        } else {
+            flow.confirmExpires = expires;
+            cb(null, flow);
+        }
+    });
+};
+
+var fnServiceUserCreateConfirm = function (flow, cb) {
+    var reqArgs = {
+        id: flow.confirmId,
+        userId: flow.userId,
+        expires: flow.confirmExpires
+    };
+    flow.env.dal.serviceUserCreateResetPasswordConfirmData(reqArgs, function (err) {
+        if (err) {
+            cb(errBuilder(dErr.INTERNAL_ERROR, err));
+        } else {
+            cb(null, flow);
+        }
+    });
+};
+
+var fnSendConfirmInfo = function (flow, cb) {
+    var reqArgs = {
+        id: flow.confirmId,
+        expires: flow.confirmExpires
+    };
+    flow.env.emailSender.sendServiceUserResetPasswordConfirmLink(reqArgs, function(err) {
+        if (err) {
+            cb(errBuilder(dErr.INTERNAL_ERROR, err));
+        } else {
+            cb(null, flow);
+        }
+    });
+};
+
+var fnGenerateResponse = function (flow, cb) {
+    var r = {
+        id: flow.confirmId,
+        expires: flow.confirmExpires
+    };
+    cb(null, r);
+};
+
+
+module.exports = fnExecute;
