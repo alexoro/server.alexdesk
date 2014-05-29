038fd4ad7be54e3d4e9a080effd5ea2fe0090d67
Sorokin Alexander <uas.sorokin@gmail.com>
2014-05-29 23:18:46 +0400

    serviceUser_resetPasswordLookup refactoring

--- bll-impl/src/api/_api_serviceUser_resetPasswordLookup.js
+++ bll-impl/src/api/_api_serviceUser_resetPasswordLookup.js
@@ -16,104 +16,26 @@ var validate = require('./_validation');
 var filter = require('./_filter');
 
 
-var _validateArgsHasErrors = function(env, args) {
-    var dErr = domain.errors;
-
-    if (!args) {
-        return errBuilder(dErr.INVALID_PARAMS, 'Arguments is not defined');
-    }
-    if (typeof args !== 'object') {
-        return errBuilder(dErr.INVALID_PARAMS, 'Arguments is not a object');
-    }
-
-    if (args.confirmToken === undefined) {
-        return errBuilder(dErr.INVALID_PARAMS, 'Confirm token is not defined');
-    }
-    if (!validate.guid(args.confirmToken)) {
-        return errBuilder(dErr.INVALID_PARAMS, 'Confirm token is invalid');
-    }
-};
-
-var _create = function(env, args, next) {
+var fnExecute = function (env, args, next) {
     var fnStack = [
         function(cb) {
             var flow = {
                 args: args,
                 env: env,
                 confirmData: null,
-                login: null
+                login: null,
+                result: null
             };
             cb(null , flow);
         },
-
-        function(flow, cb) {
-            var reqArgs = {
-                confirmToken: flow.args.confirmToken
-            };
-            flow.env.dal.fetchUserResetPasswordConfirmData(reqArgs, function(err, confirmData) {
-                if (err) {
-                    cb(errBuilder(dErr.INTERNAL_ERROR, err));
-                } else if (!confirmData) {
-                    cb(errBuilder(dErr.INVALID_OR_EXPIRED_TOKEN, 'Specified token is not found'));
-                } else {
-                    flow.confirmData = confirmData;
-                    cb(null, flow);
-                }
-            });
-        },
-        function (flow, cb) {
-            flow.env.dal.serviceUserIsExists({userId: flow.confirmData.userId}, function (err, isExists) {
-                if (err) {
-                    cb(errBuilder(dErr.INTERNAL_ERROR, err));
-                } else if (!isExists) {
-                    cb(errBuilder(dErr.USER_NOT_FOUND, 'User is not exists'));
-                } else {
-                    cb(null, flow);
-                }
-            });
-        },
-        function(flow, cb) {
-            flow.env.dal.serviceUserIsConfirmed({userId: flow.confirmData.userId}, function (err, isConfirmed) {
-                if (err) {
-                    cb(errBuilder(dErr.INTERNAL_ERROR, err));
-                } else if (!isConfirmed) {
-                    cb(errBuilder(dErr.USER_NOT_CONFIRMED, 'User is not confirmed'));
-                } else {
-                    cb(null, flow);
-                }
-            });
-        },
-
-        function(flow, cb) {
-            flow.env.currentTimeProvider.getCurrentTime(function (err, currentTime) {
-                if (err) {
-                    cb(errBuilder(dErr.INTERNAL_ERROR, err));
-                } else if (!(currentTime instanceof Date)) {
-                    cb(errBuilder(dErr.INTERNAL_ERROR, 'Current time returned not a Date'));
-                } else {
-                    flow.currentTime = currentTime;
-                    cb(null, flow);
-                }
-            });
-        },
-        function(flow, cb) {
-            if (flow.currentTime.getTime() >= flow.confirmData.expires) {
-                cb(errBuilder(dErr.INVALID_OR_EXPIRED_TOKEN, 'Token is expired'));
-            } else {
-                cb(null, flow);
-            }
-        },
-
-        function (flow, cb) {
-            flow.env.dal.getUserLoginById({userId: flow.confirmData.userId}, function (err, login) {
-                if (err) {
-                    cb(errBuilder(dErr.INTERNAL_ERROR, err));
-                } else {
-                    flow.login = login;
-                    cb(null, flow);
-                }
-            });
-        }
+        fnValidate,
+        fnServiceUserGetResetPasswordConfirmData,
+        fnServiceUserIsExists,
+        fnServiceUserIsConfirmed,
+        fnGetCurrentTIme,
+        fnConfirmIsExpired,
+        fnServiceUserGetLoginByUserId,
+        fnGenerateResult
     ];
 
     async.waterfall(
@@ -122,18 +44,113 @@ var _create = function(env, args, next) {
             if (err) {
                 next(err);
             } else {
-                next(null, {login: flow.login});
+                next(null, flow.result);
             }
         }
     );
 };
 
 
-module.exports = function(env, args, next) {
-    var argsError = _validateArgsHasErrors(env, args);
-    if (argsError) {
-        next(argsError, null);
+var fnValidate = function (flow, cb) {
+    if (!flow.args) {
+        return cb(errBuilder(dErr.INVALID_PARAMS, 'Arguments is not defined'));
+    }
+    if (typeof flow.args !== 'object') {
+        return cb(errBuilder(dErr.INVALID_PARAMS, 'Arguments is not a object'));
+    }
+
+    if (flow.args.confirmToken === undefined) {
+        return cb(errBuilder(dErr.INVALID_PARAMS, 'Confirm token is not defined'));
+    }
+    if (!validate.guid(flow.args.confirmToken)) {
+        return cb(errBuilder(dErr.INVALID_PARAMS, 'Confirm token is invalid'));
+    }
+
+    return cb(null, flow);
+};
+
+var fnServiceUserGetResetPasswordConfirmData = function (flow, cb) {
+    var reqArgs = {
+        confirmToken: flow.args.confirmToken
+    };
+    flow.env.dal.fetchUserResetPasswordConfirmData(reqArgs, function(err, confirmData) {
+        if (err) {
+            cb(errBuilder(dErr.INTERNAL_ERROR, err));
+        } else if (!confirmData) {
+            cb(errBuilder(dErr.INVALID_OR_EXPIRED_TOKEN, 'Specified token is not found'));
+        } else {
+            flow.confirmData = confirmData;
+            cb(null, flow);
+        }
+    });
+};
+
+var fnServiceUserIsExists = function (flow, cb) {
+    var reqArgs = {
+        userId: flow.confirmData.userId
+    };
+    flow.env.dal.serviceUserIsExists(reqArgs, function (err, isExists) {
+        if (err) {
+            cb(errBuilder(dErr.INTERNAL_ERROR, err));
+        } else if (!isExists) {
+            cb(errBuilder(dErr.USER_NOT_FOUND, 'User is not exists'));
+        } else {
+            cb(null, flow);
+        }
+    });
+};
+
+var fnServiceUserIsConfirmed = function (flow, cb) {
+    var reqArgs = {userId: flow.confirmData.userId};
+    flow.env.dal.serviceUserIsConfirmed(reqArgs, function (err, isConfirmed) {
+        if (err) {
+            cb(errBuilder(dErr.INTERNAL_ERROR, err));
+        } else if (!isConfirmed) {
+            cb(errBuilder(dErr.USER_NOT_CONFIRMED, 'User is not confirmed'));
+        } else {
+            cb(null, flow);
+        }
+    });
+};
+
+var fnGetCurrentTIme = function (flow, cb) {
+    flow.env.currentTimeProvider.getCurrentTime(function (err, currentTime) {
+        if (err) {
+            cb(errBuilder(dErr.INTERNAL_ERROR, err));
+        } else if (!(currentTime instanceof Date)) {
+            cb(errBuilder(dErr.INTERNAL_ERROR, 'Current time returned not a Date'));
+        } else {
+            flow.currentTime = currentTime;
+            cb(null, flow);
+        }
+    });
+};
+
+var fnConfirmIsExpired = function (flow, cb) {
+    if (flow.currentTime.getTime() >= flow.confirmData.expires) {
+        cb(errBuilder(dErr.INVALID_OR_EXPIRED_TOKEN, 'Token is expired'));
     } else {
-        _create(env, args, next);
+        cb(null, flow);
     }
 };
+
+var fnServiceUserGetLoginByUserId = function (flow, cb) {
+    flow.env.dal.getUserLoginById({userId: flow.confirmData.userId}, function (err, login) {
+        if (err) {
+            cb(errBuilder(dErr.INTERNAL_ERROR, err));
+        } else {
+            flow.login = login;
+            cb(null, flow);
+        }
+    });
+};
+
+var fnGenerateResult = function (flow, cb) {
+    flow.result = {
+        login: flow.login
+    };
+    cb(null, flow);
+};
+
+
+module.exports = fnExecute;
