3d0940f7f9bc02c4bf4199833744366570d22aea
Sorokin Alexander <uas.sorokin@gmail.com>
2014-06-12 12:21:18 +0400

    Clearings. Ready to implement api

--- dal-impl/test/_cfg.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by UAS on 27.04.2014.
- */
-
-"use strict";
-
-
-module.exports.dsn = "postgres://uas:488098@192.168.127.129:5432/test";
---
+++ dal-impl/test/api_appCreate.js
@@ -0,0 +1,25 @@
+/**
+ * Created by UAS on 12.06.2014.
+ */
+
+"use strict";
+
+
+var assert = require('chai').assert;
+
+var mock = require('./mock');
+
+
+describe('DAL::appCreate', function () {
+
+    it('Must work', function (doneTest) {
+        var api = mock.newApiWithMock().api;
+        mock.executeOnClearDb(function (doneExecute) {
+            api.appCreate({}, function (err) {
+                assert.isNull(err);
+                doneExecute();
+            });
+        }, doneTest);
+    });
+
+});
