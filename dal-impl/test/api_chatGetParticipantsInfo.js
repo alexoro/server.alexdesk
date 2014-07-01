86762ec42f77e33f061da8cac6e0277d88f99d0a
Sorokin Alexander <uas.sorokin@gmail.com>
2014-07-02 01:37:54 +0400

    Tests for some c* methods

---
+++ dal-impl/test/api_chatGetParticipantsInfo.js
@@ -0,0 +1,86 @@
+/**
+ * Created by UAS on 12.06.2014.
+ */
+
+"use strict";
+
+
+var assert = require('chai').assert;
+var async = require('async');
+
+var domain = require('../src/').domain;
+var dErr = domain.errors;
+
+var mock = require('./mock');
+
+
+var argsBuilder = function(override) {
+    if (!override) {
+        override = {};
+    }
+    return {
+        chatId: override.chatId === undefined ? '1' : override.chatId
+    };
+};
+
+var invalidArgsCallback = function (done) {
+    return function (err) {
+        if (err && err.type === dErr.INVALID_PARAMS) {
+            done();
+        } else if (err) {
+            done(err);
+        } else {
+            done(new Error('Application was created with invalid param'));
+        }
+    };
+};
+
+
+describe('DAL::chatGetParticipantsInfo', function () {
+
+    it('Must not pass invalid chatId', function (doneTest) {
+        var api = mock.newApiWithMock().api;
+        mock.executeOnClearDb(function (doneExecute) {
+            var fnStack = [
+                function (cb) {
+                    api.chatGetParticipantsInfo(argsBuilder({chatId: {}}), cb);
+                },
+                function (cb) {
+                    api.chatGetParticipantsInfo(argsBuilder({chatId: null}), cb);
+                },
+                function (cb) {
+                    api.chatGetParticipantsInfo(argsBuilder({chatId: '-1'}), cb);
+                },
+                function (cb) {
+                    api.chatGetParticipantsInfo(argsBuilder({chatId: 1}), cb);
+                }
+            ];
+            async.series(fnStack, invalidArgsCallback(doneExecute));
+        }, doneTest);
+    });
+
+    it('Must return valid result', function (doneTest) {
+        var api = mock.newApiWithMock().api;
+        mock.executeOnClearDb(function (doneExecute) {
+            var reqArgs = argsBuilder();
+            api.chatGetParticipantsInfo(reqArgs, function (err, participants) {
+                if (err) {
+                    return doneExecute(err);
+                }
+                var expected = [
+                    {
+                        id: '1',
+                        type: 1
+                    },
+                    {
+                        id: '2',
+                        type: 2
+                    }
+                ];
+                assert.deepEqual(expected, participants, 'Expected and received result are not match');
+                doneExecute();
+            });
+        }, doneTest);
+    });
+
+});
