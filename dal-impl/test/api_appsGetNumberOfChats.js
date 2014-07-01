963aa49f8923f267427bd8ee7af4bb6c19c8d159
Sorokin Alexander <uas.sorokin@gmail.com>
2014-07-01 23:54:03 +0400

    appsGetNumberOfChats tests are implememnted

---
+++ dal-impl/test/api_appsGetNumberOfChats.js
@@ -0,0 +1,99 @@
+/**
+ * Created by UAS on 12.06.2014.
+ */
+
+"use strict";
+
+
+var assert = require('chai').assert;
+var async = require('async');
+
+var domain = require('../src/').domain;
+var dErr = domain.errors;
+
+var mock = require('./mock');
+
+
+var argsBuilder = function(override) {
+    if (!override) {
+        override = {};
+    }
+    return {
+        appIds: override.appIds === undefined ? ['1'] : override.appIds
+    };
+};
+
+var invalidArgsCallback = function (done) {
+    return function (err) {
+        if (err && err.type === dErr.INVALID_PARAMS) {
+            done();
+        } else if (err) {
+            done(err);
+        } else {
+            done(new Error('Application was created with invalid param'));
+        }
+    };
+};
+
+
+describe('DAL::appsGetNumberOfChats', function () {
+
+    it('Must not pass invalid id', function (doneTest) {
+        var api = mock.newApiWithMock().api;
+        mock.executeOnClearDb(function (doneExecute) {
+            var fnStack = [
+                function (cb) {
+                    api.appsGetNumberOfChats(argsBuilder({appIds: {}}), cb);
+                },
+                function (cb) {
+                    api.appsGetNumberOfChats(argsBuilder({appIds: null}), cb);
+                },
+                function (cb) {
+                    api.appsGetNumberOfChats(argsBuilder({appIds: [-1]}), cb);
+                },
+                function (cb) {
+                    api.appsGetNumberOfChats(argsBuilder({appIds: ['-1']}), cb);
+                },
+                function (cb) {
+                    api.appsGetNumberOfChats(argsBuilder({appIds: [null]}), cb);
+                },
+            ];
+            async.series(fnStack, invalidArgsCallback(doneExecute));
+        }, doneTest);
+    });
+
+    it('Must return error if some application is not exists', function (doneTest) {
+        var api = mock.newApiWithMock().api;
+        mock.executeOnClearDb(function (doneExecute) {
+            var reqArgs = argsBuilder({appIds: ['1000']});
+            api.appsGetNumberOfChats(reqArgs, function (err) {
+                if (err && err.number === dErr.APP_IS_NOT_FOUND) {
+                    doneExecute();
+                } else if (err) {
+                    doneExecute(err);
+                } else {
+                    doneExecute(new Error('Method was executed with not existing app'));
+                }
+            });
+        }, doneTest);
+    });
+
+    it('Must return valid result', function (doneTest) {
+        var api = mock.newApiWithMock().api;
+        mock.executeOnClearDb(function (doneExecute) {
+            var reqArgs = argsBuilder();
+            api.appsGetNumberOfChats(reqArgs, function (err, result) {
+                if (err) {
+                    return doneExecute(err);
+                }
+
+                var expected = {
+                    '1': 3
+                };
+                assert.deepEqual(result, expected, 'Expected and received results are not match');
+                doneExecute();
+            });
+        }, doneTest);
+    });
+
+});
