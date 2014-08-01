2f706094b5b84365322fd000c2305355b90f1a9e
Sorokin Alexander <uas.sorokin@gmail.com>
2014-08-01 22:31:46 +0400

    Domain is deleted from rabbitmq-to-bll-impl

--- rabbitmq-to-bll-impl/src/cmd-to-bll/index.js
+++ rabbitmq-to-bll-impl/src/cmd-to-bll/index.js
@@ -5,11 +5,6 @@
 "use strict";
 
 
-var domain = require('../domain/');
-
-var errBuilder = require('./_errorBuilder');
-
-
 var Mapper = function () {
     this._ver2bll = {};
 };
@@ -28,31 +23,31 @@ Mapper.prototype.executeBllMethodFromEncodedReturnEncoded = function (encoded, c
     try {
         json = JSON.parse(encoded);
         if (!json) {
-            return cb(errBuilder(domain.errors.INTERNAL_ERROR, 'Message from queue is not a object: ' + encoded));
+            return cb(new Error('Message from queue is not a object: ' + encoded));
         }
     } catch (ex) {
-        return cb(errBuilder(domain.errors.INTERNAL_ERROR, 'Unable to parse message from queue: ' + encoded));
+        return cb(new Error('Unable to parse message from queue: ' + encoded));
     }
 
     if (!json.version) {
-        return cb(errBuilder(domain.errors.INTERNAL_ERROR, 'BLL version is not defined in message from queue: ' + encoded));
+        return cb(new Error('BLL version is not defined in message from queue: ' + encoded));
     }
     if (!json.method) {
-        return cb(errBuilder(domain.errors.INTERNAL_ERROR, 'Method is not defined in message from queue: ' + encoded));
+        return cb(new Error('Method is not defined in message from queue: ' + encoded));
     }
     if (!json.args) {
-        return cb(errBuilder(domain.errors.INTERNAL_ERROR, 'Args is not defined in message from queue: ' + encoded));
+        return cb(new Error('Args is not defined in message from queue: ' + encoded));
     }
 
     var bll = this.getBllForApiVersion(json.version);
     if (!bll) {
-        return cb(errBuilder(domain.errors.INTERNAL_ERROR, 'BLL is not defined for message from queue: ' + encoded));
+        return cb(new Error('BLL is not defined for message from queue: ' + encoded));
     }
     if (!bll[json.method]) {
-        return cb(errBuilder(domain.errors.INTERNAL_ERROR, 'BLL is not supporting method (not defined) for message from queue: ' + encoded));
+        return cb(new Error('BLL is not supporting method (not defined) for message from queue: ' + encoded));
     }
     if (typeof bll[json.method] !== 'function') {
-        return cb(errBuilder(domain.errors.INTERNAL_ERROR, 'BLL is not supporting method (not a method) for message from queue: ' + encoded));
+        return cb(new Error('BLL is not supporting method (not a method) for message from queue: ' + encoded));
     }
 
     return bll[json.method](json.args, function (err, result) {
