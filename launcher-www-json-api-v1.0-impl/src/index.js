cef93b4f0077bfbdaa0da60d762ac83531fcf5bc
Sorokin Alexander <uas.sorokin@gmail.com>
2014-08-05 19:32:11 +0400

    Cluster now is used for launching

--- launcher-www-json-api-v1.0-impl/src/index.js
+++ launcher-www-json-api-v1.0-impl/src/index.js
@@ -5,6 +5,9 @@
 "use strict";
 
 
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
+
 var Bll = require('../../bll-impl').api;
 var Dal = require('../../dal-impl').api;
 var Uuid = require('../../uuid-generator');
@@ -37,4 +40,18 @@ var jsonRpcServer = new JsonRpcServer({
     log: null
 });
 
-jsonRpcServer.start();
+
+if (cluster.isMaster) {
+    // Fork workers.
+    for (var i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', function(worker, code, signal) {
+        console.log('worker ' + worker.process.pid + ' died');
+    });
+} else {
+    // Workers can share any TCP connection
+    // In this case its a HTTP server
+    jsonRpcServer.start();
+}
